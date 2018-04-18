78692779a778f91902f5dd795ddafccd6f5a567c
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-04-18 10:59:14 -0700

    format data

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -6,6 +6,9 @@ import List from 'List.js';
 class Funding extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			rounds: [],
+		}
 	}
 
 	componentDidMount() {
@@ -15,31 +18,63 @@ class Funding extends React.Component {
 
 		var userList = new List('funding_rounds', options);
 
-		//Call fetchData()
+		this.fetchData();
 	}
 
 	fetchData() {
-		//fetch database values for a specific company
-
-		//get request to api
+		$.ajax({
+			method: 'GET',
+			url: '/api/funding_round',
+			contentType: 'application/json',
+			success: (data) => {
+				console.log('success');
+				this.setState({rounds: data});
+			}
+		})
 	}
 
 	render() {
 
-		// var tableRows = [];
-		// var data = [];
+		var tableRows = [];
 
-		// tableRows = data.map(function(row) {
-		// 	return (
-	 //      <tr>
-	 //        <td className="Announced Date" data-value='20170810'>Aug 10, 2017</td>
-	 //        <td className="Transaction Name">Series D - Coinbase</td>
-	 //        <td className="Number of Investors">13</td>
-	 //        <td className="Money Raised" data-value='0108100000'>$108.1M</td>
-	 //        <td className="Lead Investors">IVP (Institutional Venture Partners)</td>
-	 //      </tr>
-		// 	)
-		// })
+		tableRows = this.state.rounds.map(function(row) {
+			var date = row.announcedDate.slice(0, 4) + row.announcedDate.slice(5, 7) + row.announcedDate.slice(8, 10);
+			
+			var longDate = (new Date(row.announcedDate)).toString();
+			var formattedDate = longDate.substring(4,10) + ',' + longDate.substring(10,15);
+
+			var formattedTransactionName = row.transactionName.substring(0,1).toUpperCase() + row.transactionName.substring(1);
+
+			if (formattedTransactionName.includes('Series')) {
+				formattedTransactionName = formattedTransactionName.substring(0,7) +
+																	 formattedTransactionName.substring(7,8).toUpperCase() +
+																	 formattedTransactionName.substring(8);
+			}
+
+			var amountRaised = row.moneyRaised.toString();
+			var amountData = amountRaised
+			for (var i = 0; i < (12 - amountRaised.length); i++) {
+				amountData = '0' + amountData;
+			}
+
+			if (amountRaised.length >= 4 && amountRaised.length <=6) {
+				var formattedAmountRaised = '$' + (row.moneyRaised / 1000).toString() + 'K';
+			} else if (amountRaised.length >= 7 && amountRaised.length <=9) {
+				var formattedAmountRaised = '$' + (row.moneyRaised / 1000000).toString() + 'M';
+			} else if (amountRaised.length >= 10 && amountRaised.length <=12) {
+				var formattedAmountRaised = '$' + (row.moneyRaised / 1000000000).toString() + 'B';
+			}
+
+			return (
+	      <tr>
+	        <td className="Announced Date" data-value={date}>{formattedDate}</td>
+	        <td className="Transaction Name">{formattedTransactionName}</td>
+	        <td className="Number of Investors">{row.numInvestors}</td>
+	        <td className="Money Raised" data-value={amountData}>{formattedAmountRaised}</td>
+	        <td className="Lead Investors">{row.leadInvestor}</td>
+	      </tr>
+			)
+		})
 
 		return (
 			<div id="funding_rounds">
@@ -61,34 +96,7 @@ class Funding extends React.Component {
 
 			  <table>
 			    <tbody className="list">
-			      <tr>
-			        <td className="Announced Date" data-value='20170810'>Aug 10, 2017</td>
-			        <td className="Transaction Name">Series D - Coinbase</td>
-			        <td className="Number of Investors">13</td>
-			        <td className="Money Raised" data-value='0108100000'>$108.1M</td>
-			        <td className="Lead Investors">IVP (Institutional Venture Partners)</td>
-			      </tr>
-			      <tr>
-			        <td className="Announced Date" data-value='20160707'>Jul 7, 2016</td>
-			        <td className="Transaction Name">Series C - Coinbase</td>
-			        <td className="Number of Investors">13</td>
-			        <td className="Money Raised" data-value='0075000000'>$75M</td>
-			        <td className="Lead Investors">Andreessen Horowitz</td>
-			      </tr>
-			      <tr>
-			        <td className="Announced Date" data-value='20151210'>Dec 10, 2015</td>
-			        <td className="Transaction Name">Series A - Coinbase</td>
-			        <td className="Number of Investors">2</td>
-			        <td className="Money Raised" data-value='0010000000'>$10M</td>
-			        <td className="Lead Investors">IVP (Institutional Venture Partners)</td>
-			      </tr>
-			      <tr>
-			        <td className="Announced Date" data-value='20150110'>Jan 10, 2015</td>
-			        <td className="Transaction Name">Seed Round - Coinbase</td>
-			        <td className="Number of Investors">5</td>
-			        <td className="Money Raised" data-value='0000600000'>$600K</td>
-			        <td className="Lead Investors">Lowercase Capital</td>
-			      </tr>
+			  		{tableRows}
 			    </tbody>
 			  </table>
 			</div>
@@ -97,3 +105,60 @@ class Funding extends React.Component {
 }
 
 ReactDOM.render(<Funding />, document.getElementById('funding_status'));
+
+
+
+			      // <tr>
+			      //   <td className="Announced Date" data-value='20170810'>Aug 10, 2017</td>
+			      //   <td className="Transaction Name">Series D - Coinbase</td>
+			      //   <td className="Number of Investors">13</td>
+			      //   <td className="Money Raised" data-value='0108100000'>$108.1M</td>
+			      //   <td className="Lead Investors">IVP (Institutional Venture Partners)</td>
+			      // </tr>
+			      // <tr>
+			      //   <td className="Announced Date" data-value='20160707'>Jul 7, 2016</td>
+			      //   <td className="Transaction Name">Series C - Coinbase</td>
+			      //   <td className="Number of Investors">13</td>
+			      //   <td className="Money Raised" data-value='0075000000'>$75M</td>
+			      //   <td className="Lead Investors">Andreessen Horowitz</td>
+			      // </tr>
+			      // <tr>
+			      //   <td className="Announced Date" data-value='20151210'>Dec 10, 2015</td>
+			      //   <td className="Transaction Name">Series A - Coinbase</td>
+			      //   <td className="Number of Investors">2</td>
+			      //   <td className="Money Raised" data-value='0010000000'>$10M</td>
+			      //   <td className="Lead Investors">IVP (Institutional Venture Partners)</td>
+			      // </tr>
+			      // <tr>
+			      //   <td className="Announced Date" data-value='20150110'>Jan 10, 2015</td>
+			      //   <td className="Transaction Name">Seed Round - Coinbase</td>
+			      //   <td className="Number of Investors">5</td>
+			      //   <td className="Money Raised" data-value='0000600000'>$600K</td>
+			      //   <td className="Lead Investors">Lowercase Capital</td>
+			      // </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
